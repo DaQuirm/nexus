93942f7385066cdfdce8bc2c9fbb46622f0293ba
DaQuirm <leo.daquirm@gmail.com>
2013-02-06 17:16:28 +0300

    Add bind method nx.Property

--- src/property.js
+++ src/property.js
@@ -1,13 +1,33 @@
 'use strict';
 
-window.nx = window.nx || {}
+window.nx = window.nx || {};
 
-window.nx.Property = function() {
+nx.Property = function(options) {
 	var _value;
+	options = options || {};
+	this.bindings = {};
+	this.bindingIndex = 0;
 
 	Object.defineProperty(this, 'value', {
 		enumerable : true,
 		get: function() { return _value; },
-		set: function(value) { _value = value; }
+		set: function(value) {
+			_value = value;
+			if (typeof options.set !== 'undefined') {
+				options.set.call(this, value);
+			}
+			Object.keys(this.bindings).forEach(function(binding) {
+				this.bindings.sync();
+			});
+		}
 	});
-}
+};
+
+nx.Property.prototype.bind = function(target, mode, sTransform, tTransform) {
+	this.bindings[this.bindingIndex++] = new nx.Binding(
+		bindingIndex,
+		mode,
+		sTransform,
+		tTransform
+	);
+};
