27d02defa01deceb603c8cb0bfa6470461e7394d
daquirm <daquirm@daquirm-VirtualBox.(none)>
2013-02-14 17:36:30 +0300

    Add nx.Event spec

---
+++ test/event.spec.js
@@ -0,0 +1,50 @@
+describe('nx.Event', function() {
+
+	describe('constructor', function() {
+		it('creates an event instance', function() {
+			var evt = new nx.Event();
+			evt.should.be.an('object');
+			evt.should.be.an.instanceof(nx.Event);
+		});
+	});
+
+	describe('trigger', function() {
+		it('fires the event calling all added handlers passing its argument to them', function() {
+			var evt = new nx.Event();
+			var triggerCount = 0;
+			evt.add(function(value){
+				value.should.equal('cellar door');
+				triggerCount++;
+			});
+			evt.trigger('cellar door');
+			triggerCount.should.equal(1);
+		});
+	});
+
+	describe('add', function() {
+		it('adds a handler function with an optional name parameter', function() {
+			var evt = new nx.Event();
+			var triggerCount = 0;
+			evt.add(function(){
+				triggerCount++;
+			}, 'event-handler');
+			evt.trigger();
+			triggerCount.should.equal(1);
+		});
+	});
+
+	describe('remove', function() {
+		it('removes a handler from the event handler list by its name', function() {
+			var evt = new nx.Event();
+			var triggerCount = 0;
+			evt.add(function(){
+				triggerCount++;
+			}, 'event-handler');
+			evt.trigger();
+			triggerCount.should.equal(1);
+			evt.remove('event-handler');
+			evt.trigger();
+			triggerCount.should.not.equal(2);
+		});
+	});
+});
