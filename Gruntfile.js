ca31ad06dfe9d8813274ee7cb2b19c025a518b0b
DaQuirm <leo.daquirm@gmail.com>
2013-02-16 22:52:43 +0300

    Add concatenated build for linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,37 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-wrap');
 
-  grunt.initConfig({
-    jshint: {
-        all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
-    }
-  });
+	grunt.initConfig({
+		concat: {
+			source: {
+				src: 'src/**/*.js',
+				dest: 'build/nexus.js'
+			}
+		},
+		wrap: {
+			build: {
+				src: 'build/nexus.js',
+				dest: '',
+				wrapper: ['(function() {\n\t\'use strict\';\n', '\n})();\n']
+			}
+		},
+		jshint: {
+			build: ['build/nexus.js'],
+			source: ['src/**/*.js'],
+			test: ['test/**/*.js'],
+			options: {
+				boss: true,
+				browser: true,
+				forin: false,
+				strict: true,
+				globals: {
+					'nx': true
+				},
+			}
+		}
+	});
 
-  grunt.registerTask('default', ['jshint:all']);
+	grunt.registerTask('default', ['concat:source', 'wrap:build', 'jshint:build']);
 };
