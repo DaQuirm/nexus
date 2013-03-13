3132c789e3583a7c61de7c20c18edbe93c0ecb80
DaQuirm <leo.daquirm@gmail.com>
2013-03-13 17:56:13 +0300

    Add nxt.BindingRenderer draft implementation

---
+++ src/nxt/renderers/binding-renderer.js
@@ -0,0 +1,17 @@
+window.nxt = window.nxt || {};
+
+nxt.BindingRenderer = function(element) {
+	var _this = this;
+	this.element = element;
+	this.property = new nx.Property();
+	this.property.onvalue.add(function(data) {
+		if (!_this.contentRenderer || !_this.contentRenderer instanceof nxt[data.type+'Renderer']) {
+			_this.contentRenderer = new nxt[data.type+'Renderer'](_this.element);
+		}
+		_this.contentRenderer.render(data);
+	});
+};
+
+nxt.BindingRenderer.prototype.render = function(binding) {
+	this.binding = binding.property.bind(this.property, binding.mode, binding.conversion);
+};
