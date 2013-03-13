7b0cf917ad28e7a6c1494208a1bc96c70731a26a
DaQuirm <leo.daquirm@gmail.com>
2013-03-13 15:27:32 +0300

    Add nxt.EventRenderer spec

---
+++ test/nxt/renderers/event-renderer.spec.js
@@ -0,0 +1,22 @@
+describe('nxt.EventRenderer', function() {
+	'use strict';
+
+	describe('constructor', function() {
+		it('creates an event managing instance by event name and handler', function() {
+			var element = document.createElement('div');
+			var renderer = new nxt.EventRenderer(element);
+			renderer.element.should.equal(element);
+		});
+	});
+
+	describe('render', function() {
+		it('attaches an event handler to the element', function() {
+			var element = document.createElement('div');
+			var renderer = new nxt.EventRenderer(element);
+			var handler = chai.spy();
+			renderer.render(nxt.Event('click', handler));
+			element.click();
+			handler.should.have.been.called();
+		});
+	});
+});
