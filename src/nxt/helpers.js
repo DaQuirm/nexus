944740648a7b33013a1b6fdecbe8b5d0c13079f3
DaQuirm <leo.daquirm@gmail.com>
2013-03-13 17:26:04 +0300

    Add nxt helpers draft implementation

---
+++ src/nxt/helpers.js
@@ -0,0 +1,41 @@
+window.nxt = window.nxt || {};
+
+nxt.Attr = function(name, value) {
+	return {
+		name: name,
+		value: value,
+		type: 'Attr'
+	};
+};
+
+nxt.Text = function(text) {
+	return {
+		text: text,
+		node: document.createTextNode(text),
+		type: 'Text'
+	};
+};
+
+nxt.Event = function(name, handler) {
+	return {
+		name: name,
+		handler: handler,
+		type: 'Event'
+	};
+};
+
+nxt.Element = function() {
+	return {
+
+	};
+};
+
+nxt.Binding = function(property, conversion, mode) {
+	return {
+		property: property,
+		conversion: conversion,
+		mode: mode || '->',
+		type: 'Binding'
+	};
+};
+
