6e58e8dda3ed8bf21c145f252450456864e7c38b
DaQuirm <leo.daquirm@gmail.com>
2013-03-13 17:34:53 +0300

    Add nxt.AttrRenderer implementation

--- src/nxt/renderers/attr-renderer.js
+++ src/nxt/renderers/attr-renderer.js
@@ -1,9 +1,9 @@
 window.nxt = window.nxt || {};
 
-nxt.AttrRenderer = function() {
-
+nxt.AttrRenderer = function(element) {
+	this.element = element;
 };
 
-nxt.AttrRenderer.prototype.render = function() {
-
+nxt.AttrRenderer.prototype.render = function(attr) {
+	this.element.setAttribute(attr.name, attr.value);
 };
