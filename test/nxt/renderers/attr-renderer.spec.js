b90496f1208e8124d846280f08a2df5ea1a65861
DaQuirm <leo.daquirm@gmail.com>
2013-03-13 16:51:13 +0300

    Update nxt.AttrRenderer spec

--- test/nxt/renderers/attr-renderer.spec.js
+++ test/nxt/renderers/attr-renderer.spec.js
@@ -3,20 +3,18 @@ describe('nxt.AttrRenderer', function() {
 
 	describe('constructor', function() {
 		it('creates a DOM atribute rendering instance by attribute name and value', function() {
-			var renderer = new nxt.AttrRenderer('class', 'large-text');
-			renderer.name.should.equal('class');
-			renderer.value.should.equal('large-text');
+			var element = document.createElement('a');
+			var renderer = new nxt.AttrRenderer(element);
+			renderer.element.should.equal(element);
 		});
 	});
 
 	describe('render', function() {
 		it('sets an attribute of a DOM element', function() {
 			var link = document.createElement('a');
-			var renderer = new nxt.AttrRenderer('class', 'large-text');
-			renderer.render(link);
+			var renderer = new nxt.AttrRenderer(link);
+			renderer.render(nxt.Attr('class', 'large-text'));
 			link.getAttribute('class').should.equal('large-text');
 		});
-
-		it('stores link to the attribute node created');
 	});
 });
