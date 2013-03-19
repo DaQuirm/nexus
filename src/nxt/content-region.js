59ee47c2f734dff006aa61344deb55294de1c6bf
DaQuirm <leo.daquirm@gmail.com>
2013-03-19 10:41:30 +0300

    Update nxt.ContentRegion (WIP)

--- src/nxt/content-region.js
+++ src/nxt/content-region.js
@@ -10,6 +10,9 @@ nxt.ContentRegion.prototype.add = function(item) {
 	var id = this.items.length;
 	var _this = this;
 	this.items.push(item);
+	if (typeof this.insertReference !== 'undefined') {
+		item.insertReference = this.insertReference;
+	}
 	item.property.onvalue.add(function(value) {
 		var wasVisible = _this.visibility[id];
 		if (typeof value !== 'undefined' && !wasVisible) {
@@ -22,7 +25,14 @@ nxt.ContentRegion.prototype.add = function(item) {
 
 nxt.ContentRegion.prototype.update = function(id, visible) {
 	this.visibility[id] = visible;
-	var insertReference = visible ? this.items[id].contentRenderer.insertReference : this.items[id].insertReference;
+	var insertReference;
+	if (visible) {
+		insertReference = this.items[id].contentRenderer.content;
+	} else if (this.insertReference) {
+		insertReference = this.insertReference;
+	} else {
+		insertReference = this.items[id].insertReference;
+	}
 	for (var index = id-1; index >= 0; index--) {
 		if (this.visibility[index]) { break; }
 		this.items[index].insertReference = insertReference;
