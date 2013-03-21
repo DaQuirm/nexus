45426fb4cf2c4f358a659b4c2bf1e20caad04205
DaQuirm <leo.daquirm@gmail.com>
2013-03-21 12:32:41 +0300

    Implement nxt.CollectionRenderer

---
+++ src/nxt/renderers/collection-renderer.js
@@ -0,0 +1,47 @@
+window.nxt = window.nxt || {};
+
+nxt.CollectionRenderer = function(element) {
+	var _this = this;
+	this.element = element;
+};
+
+nxt.CollectionRenderer.prototype.render = function(data) {
+	var _this = this;
+	this.collection = data.collection;
+	this.conversion = data.conversion;
+	this.collection.onappend.add(function(evt){	_this.append(evt); });
+	this.collection.oninsertbefore.add(function(evt){ _this.insertBefore(evt); });
+	this.collection.onremove.add(function(evt){	_this.remove(evt); });
+	this.collection.onreset.add(function(evt){ _this.reset(evt); });
+};
+
+nxt.CollectionRenderer.prototype.append = function(evt) {
+	var convItems = evt.items.map(this.conversion);
+	var manager = new nxt.ContentManager(this.element);
+	nxt.ContentManager.prototype.render.apply(manager, convItems);
+};
+
+nxt.CollectionRenderer.prototype.insertBefore = function(evt) {
+	var _this = this;
+	var convItems = evt.items.map(this.conversion).forEach(function(item){
+		var renderer = new nxt[item.type+'Renderer'](_this.element);
+		renderer.insertReference = _this.element.childNodes[evt.index];
+		renderer.render(item);
+	});
+};
+
+nxt.CollectionRenderer.prototype.remove = function(evt) {
+	var _this = this;
+	evt.indexes.forEach(function(index){
+		_this.element.removeChild(_this.element.childNodes[index]);
+	});
+};
+
+nxt.CollectionRenderer.prototype.reset = function(evt) {
+	while(this.element.firstChild) {
+		this.element.removeChild(this.element.firstChild);
+	}
+	var convItems = evt.items.map(this.conversion);
+	var manager = new nxt.ContentManager(this.element);
+	nxt.ContentManager.prototype.render.apply(manager, convItems);
+};
--- test/nxt/renderers/collection-renderer.spec.js
+++ test/nxt/renderers/collection-renderer.spec.js
@@ -36,101 +36,74 @@ describe('nxt.CollectionRenderer', function() {
 	describe('append', function () {
 		it('appends rendered content to the element', function () {
 			var container = document.createElement('ul');
-			var collection = new nx.Collection({items: ['a','b']});
+			var collection = new nx.Collection();
 			var renderer = new nxt.CollectionRenderer(container);
 			renderer.render(
 				nxt.Collection(collection, function(value) {
 					return nxt.Element('li', nxt.Text(value));
 				})
 			);
+			renderer.append({items:['a','b']});
 			renderer.append({items:['cellar door']});
 			container.childNodes.length.should.equal(3);
-			container.lastChild.nodeType.should.equal(Node.TEXT_NODE);
-			container.lastChild.nodeValue.should.equal('cellar door');
+			container.lastChild.nodeName.toLowerCase().should.equal('li');
+			container.lastChild.textContent.should.equal('cellar door');
 		});
 	});
 
 	describe('remove', function () {
 		it('removes content items from the element', function () {
 			var container = document.createElement('ul');
-			var collection = new nx.Collection({items: ['a','b','c']});
+			var collection = new nx.Collection();
 			var renderer = new nxt.CollectionRenderer(container);
 			renderer.render(
 				nxt.Collection(collection, function(value) {
 					return nxt.Element('li', nxt.Text(value));
 				})
 			);
+			renderer.append({items:['a', 'b', 'c']});
 			container.childNodes.length.should.equal(3);
 			renderer.remove({items:['b'], indexes:[1]});
 			container.childNodes.length.should.equal(2);
-			container.firstChild.nodeValue.should.equal('a');
-			container.lastChild.nodeValue.should.equal('c');
+			container.firstChild.textContent.should.equal('a');
+			container.lastChild.textContent.should.equal('c');
 		});
 	});
 
 	describe('insertBefore', function () {
 		it('inserts an item before an existing item inside the element', function () {
 			var container = document.createElement('ul');
-			var collection = new nx.Collection({items: ['a', 'c']});
+			var collection = new nx.Collection();
 			var renderer = new nxt.CollectionRenderer(container);
 			renderer.render(
 				nxt.Collection(collection, function(value) {
 					return nxt.Element('li', nxt.Text(value));
 				})
 			);
-			collection.insertBefore({items: ['b'], index: 1});
+			renderer.append({items:['a', 'c']});
+			renderer.insertBefore({items: ['b'], index: 1});
 			container.childNodes.length.should.equal(3);
-			container.firstChild.nodeValue.should.equal('a');
-			container.childNodes[1].nodeValue.should.equal('b');
+			container.firstChild.textContent.should.equal('a');
+			container.childNodes[1].textContent.should.equal('b');
 		});
 	});
 
-	describe('removeAll', function () {
-		it('removes all content items from the element', function () {
+	describe('reset', function () {
+		it('removes all content items from the element and renders new items', function () {
 			var container = document.createElement('ul');
-			var collection = new nx.Collection({items: ['a','b','c']});
+			var collection = new nx.Collection();
 			var renderer = new nxt.CollectionRenderer(container);
 			renderer.render(
 				nxt.Collection(collection, function(value) {
 					return nxt.Element('li', nxt.Text(value));
 				})
 			);
-			renderer.removeAll();
-			container.childNodes.length.should.equal(0);
+			renderer.append({items: ['a','b','c']});
+			renderer.reset({items: ['d','e','f']});
+			container.childNodes.length.should.equal(3);
+			container.firstChild.textContent.should.equal('d');
+			container.childNodes[1].textContent.should.equal('e');
+			container.lastChild.textContent.should.equal('f');
 		});
-	});
-
-	it('creates renderers based on content type received from the bound property', function() {
-		var element = document.createElement('div');
-		var collection = new nx.Collection();
-		var renderer = new nxt.CollectionRenderer(element);
-		renderer.render(
-			nxt.Collection(collection, nxt.Text)
-		);
-		collection.append('cellar door');
-		renderer.contentRenderer.should.be.an.instanceof(nxt.TextRenderer);
-	});
-
-	it('passes its insert reference to contentRenderer', function() {
-		var element = document.createElement('div');
-		var movieNode = document.createTextNode('Lethal Weapon IV');
-		element.appendChild(movieNode);
-		var collection = new nx.Collection();
-		var renderer = new nxt.CollectionRenderer(element);
-		renderer.insertReference = movieNode;
-		renderer.render(
-			nxt.Collection(collection, nxt.Text)
-		);
-		collection.append('Lethal Weapon I', 'Lethal Weapon II', 'Lethal Weapon III');
-		renderer.contentRenderer.insertReference.should.equal(renderer.insertReference);
-		element.childNodes.length.should.equal(4);
-		element.childNodes[0].textContent.should.equal('Lethal Weapon I');
-		element.childNodes[0].nodeType.should.equal(Node.TEXT_NODE);
-		element.childNodes[1].textContent.should.equal('Lethal Weapon II');
-		element.childNodes[1].nodeType.should.equal(Node.TEXT_NODE);
-		element.childNodes[2].textContent.should.equal('Lethal Weapon III');
-		element.childNodes[2].nodeType.should.equal(Node.TEXT_NODE);
-		element.childNodes[3].textContent.should.equal('Lethal Weapon IV');
-		element.childNodes[3].nodeType.should.equal(Node.TEXT_NODE);
 	});
 });
