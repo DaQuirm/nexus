97ac5917ec0f46524f6e0715216b339e068be78d
DaQuirm <leo.daquirm@gmail.com>
2014-03-31 08:48:52 +0300

    Update nx.RestCollection spec (WIP)

--- test/rest/rest-collection.spec.js
+++ test/rest/rest-collection.spec.js
@@ -1,19 +1,46 @@
 describe('nx.RestCollection', function() {
 
+	var url = 'http://localhost/news/';
+
 	describe('constructor', function() {
 		it('creates a new collection model instance based on a URL', function() {
 			var url = 'http://localhost/users';
-			var model = new nx.RestCollection({ url: url });
-			model.url.should.equal(url);
+			var collection = new nx.RestCollection({ url: url });
+			collection.url.should.equal(url);
 		});
 
 		it('accepts a rest document model constructor for collection item instantiation', function() {
-			var UserModel = Object.create(nx.RestDocument);
-			var model = new nx.RestCollection({ url: url, item:  });
+			var UserModel = nx.RestDocument;
+			var collection = new nx.RestCollection({ url: url, item: UserModel });
+			collection.item.should.equal(UserModel);
 		});
 	});
 
 	describe('create', function() {
+		it('adds a model instance to the collection model with a POST request', function(done) {
+			var collection = new nx.RestCollection({ url: url, item: UserModel });
+			var request_spy = sinon.spy(collection.request);
+			var item = new nx.RestDocument();
+			collection.create(item, function() {
+				request_spy.should.have.been.caledWith({
+					url: url,
+					method: 'put',
+					data: { name: 'Samuel' }
+				});
+				done();
+			});
+		});
+	});
 
+	describe('retrieve', function() {
+		it('asynchronously gets collection data with a GET request', function(done) {
+			var request_spy = sinon.spy(model.request);
+			model.retrieve(function(data) {
+				data.should.be.json;
+				model.data.value.shoul.deep.equal(data);
+				request_spy.should.have.been.caledWith({ url: url, method: 'get' });
+				done();
+			});
+		});
 	});
 });
