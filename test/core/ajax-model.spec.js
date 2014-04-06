70dba6f3c6529bcaf3f1783b2cf57a0dd469d3f6
DaQuirm <leo.daquirm@gmail.com>
2014-04-06 13:37:39 +0300

    Implement nx.AjaxModel

--- test/core/ajax-model.spec.js
+++ test/core/ajax-model.spec.js
@@ -11,9 +11,9 @@ describe('nx.AjaxModel', function() {
 			should.exist(model);
 		});
 
-		it('initializes the data property using the first constructor parameter', function() {
-			model = new nx.AjaxModel({cellar: 'door'});
-			model.data.value.should.deep.equal({cellar: 'door'});
+		it('initializes the data property using the options parameter', function() {
+			model = new nx.AjaxModel({ data: { cellar: 'door' }});
+			model.data.value.should.deep.equal({ cellar: 'door' });
 		});
 	});
 
@@ -34,10 +34,8 @@ describe('nx.AjaxModel', function() {
 		var url = 'http://localhost/users';
 
 		beforeEach(function () {
-			server = sinon.fakeServer.create();
 			xhr = sinon.useFakeXMLHttpRequest();
-			requests = [];
-			xhr.onCreate = function (req) { requests.push(req); };
+			server = sinon.fakeServer.create();
 		});
 
 		afterEach(function () {
@@ -50,21 +48,9 @@ describe('nx.AjaxModel', function() {
 				url: url,
 				method: 'get'
 			});
-			requests.length.should.equal(1);
-			requests[0].url.should.equal(url);
-			request.method.should.equal('get');
-		});
-
-		it('sends http requests using the data property', function() {
-			model.data.value = { 'name': 'Samuel' };
-			model.request({
-				url: url,
-				method: 'post'
-			});
-			requests.length.should.equal(1);
-			requests[0].url.should.equal(url);
-			request.method.should.equal('post');
-			request.requestBody.should.deep.equal(model.data.value);
+			server.requests.length.should.equal(1);
+			server.requests[0].url.should.equal(url);
+			server.requests[0].method.should.equal('get');
 		});
 
 		it('interpolates the url using placeholders in curly braces and substitute values from model data', function() {
@@ -73,31 +59,38 @@ describe('nx.AjaxModel', function() {
 				method: 'put',
 				data: { 'name': 'Samuel', '_id':1337 }
 			});
-			requests.length.should.equal(1);
-			requests[0].url.should.equal('http://localhost/users/1337');
+			server.requests.length.should.equal(1);
+			server.requests[0].url.should.equal('http://localhost/users/1337');
 		});
 
 		it('calls a success handler if request succeeds and passes received data as the first parameter', function() {
 			var handler = sinon.spy();
+			var response = { 'response': 'cellar door' };
+			server.respondWith([
+				200,
+				{ "Content-Type": "application/json" },
+				JSON.stringify(response)
+			]);
 			model.request({
 				url: url,
 				method: 'put',
 				data: { 'name': 'Samuel' },
 				success: handler
 			});
-			var response = { 'response': 'cellar door' };
+			server.respond();
 			server.requests.length.should.equal(1);
-			server.requests[0].respond(
-				200,
-				{ "Content-Type": "application/json" },
-				JSON.stringify(response)
-			);
 			handler.should.have.been.calledWith(response);
 		});
 
 		it('calls an error handler if request fails and passes an error object as the first parameter', function() {
 			var success_handler = sinon.spy();
 			var error_handler = sinon.spy();
+			var response = { 'error': 'not found' };
+			server.respondWith([
+				404,
+				{ "Content-Type": "application/json" },
+				JSON.stringify(response)
+			]);
 			model.request({
 				url: url,
 				method: 'put',
@@ -105,16 +98,54 @@ describe('nx.AjaxModel', function() {
 				success: success_handler,
 				error: error_handler
 			});
-			var response = { 'error': 'not found' };
+			server.respond();
 			server.requests.length.should.equal(1);
-			server.requests[0].respond(
-				404,
-				{ "Content-Type": "application/json" },
-				JSON.stringify(response)
-			);
 			error_handler.should.have.been.calledWith(response);
 			success_handler.should.not.have.been.called;
 		});
 
+		it('changes model status to loading when request is sent', function() {
+			model.request({
+				url: url,
+				method: 'get'
+			});
+			model.status.value.should.equal(nx.AsyncStatus.LOADING);
+		});
+
+		it('changes model status to done when request is complete', function(done) {
+			var response = { 'response': 'cellar door' };
+			server.respondWith([
+				200,
+				{ "Content-Type": "application/json" },
+				JSON.stringify(response)
+			]);
+			model.request({
+				url: url,
+				method: 'get',
+				success: function() {
+					model.status.value.should.equal(nx.AsyncStatus.DONE);
+					done();
+				}
+			});
+			server.respond();
+		});
+
+		it('changes model status to error when request fails', function(done) {
+			var response = { 'error': 'not found' };
+			server.respondWith([
+				404,
+				{ "Content-Type": "application/json" },
+				JSON.stringify(response)
+			]);
+			model.request({
+				url: url,
+				method: 'get',
+				error: function() {
+					model.status.value.should.equal(nx.AsyncStatus.ERROR);
+					done();
+				}
+			});
+			server.respond();
+		});
 	});
 });
