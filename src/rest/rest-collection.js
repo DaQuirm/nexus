63a4890fe69fb24c2ffdaa8d4655f0212fcf60ae
DaQuirm <leo.daquirm@gmail.com>
2014-05-14 19:19:57 +0300

    Implement nx.RestCollection (WIP)

--- src/rest/rest-collection.js
+++ src/rest/rest-collection.js
@@ -1,5 +1,34 @@
 window.nx = window.nx || {};
 
 nx.RestCollection = function(options) {
+	this.url = options.url;
+	this.itemConstructor = options.item;
+	this.items = new nx.Collection();
+};
 
+nx.RestCollection.prototype = Object.create(nx.AjaxModel.prototype);
+
+nx.RestCollection.prototype.create = function(item, done) {
+	var _this = this;
+	this.request({
+		url: this.url,
+		method: 'post',
+		data: item.data.value,
+		success: function(data) {
+			_this.data.value = data;
+			done.call(null, data);
+		}
+	});
+};
+
+nx.RestCollection.prototype.retrieve = function(done) {
+	var _this = this;
+	this.request({
+		url: this.url,
+		method: 'get',
+		success: function(data) {
+			_this.data.value = data;
+			done.call(null, data);
+		}
+	});
 };
