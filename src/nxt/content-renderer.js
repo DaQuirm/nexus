145861f6545e6f0239a1586ebd1a990862e8b91b
DaQuirm <leo.daquirm@gmail.com>
2015-05-19 08:24:42 +0200

    Improve ContentRenderer.insertBefore performance

--- src/nxt/content-renderer.js
+++ src/nxt/content-renderer.js
@@ -51,13 +51,17 @@ nxt.ContentRenderer = {
 	},
 
 	insertBefore: function (data, domContext) {
-		data.items.forEach(function (item, index) {
-			var content = item.run({
+		var insertReference = domContext.content[data.index];
+		var content = data.items.map(function (item) {
+			return item.run({
 				container: domContext.container,
-				insertReference: domContext.content[data.index + index]
+				insertReference: insertReference
 			});
-			domContext.content.splice(data.index + index, 0, content);
 		});
+		[].splice.apply(
+			domContext.content,
+			[data.index, 0].concat(content)
+		);
 		return domContext.content;
 	},
 
