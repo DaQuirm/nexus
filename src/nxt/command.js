190ae5feb473448dbdd0385b0b534b1af4c3de6a
Alexander Gerasimov <leo.daquirm@gmail.com>
2015-07-22 16:07:12 +0200

    Make nxt.Command inherit from nx.Command

--- src/nxt/command.js
+++ src/nxt/command.js
@@ -1,16 +1,23 @@
+var nx = {
+	Command: require('../core/command'),
+	Utils: require('../core/utils')
+};
+
 var nxt = require('./renderers');
 
 nxt.Command = function (type, method, data) {
+	nx.Command.call(this, method, data);
 	this.type = type;
-	this.method = method;
-	this.data = data;
 };
+
+nx.Utils.mixin(nxt.Command.prototype, nx.Command.prototype);
+nxt.Command.prototype.constructor = nxt.Command;
 
 nxt.Command.prototype.run = function () {
 	this.renderer = nxt[this.type + 'Renderer'];
-	return this.renderer[this.method].apply(
-		this.renderer,
-		[this.data].concat([].slice.apply(arguments))
+	return this.apply.apply(
+		this,
+		[this.renderer].concat([].slice.apply(arguments))
 	);
 };
 
