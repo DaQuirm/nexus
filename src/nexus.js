8fd3fa34458c9ca173922bd6c3cb25dee132d993
DaQuirm <leo.daquirm@gmail.com>
2015-10-02 21:43:15 +0200

    Expose nx.Command for client-side nexus

--- src/nexus.js
+++ src/nexus.js
@@ -5,6 +5,7 @@ var nx = window.nx = {
 	Binding:       require('./core/binding'),
 	Cell:          require('./core/cell'),
 	Collection:    require('./core/collection'),
+	Command:       require('./core/command'),
 	LiveTransform: require('./core/live-transform'),
 	Mapping:       require('./core/mapping'),
 
